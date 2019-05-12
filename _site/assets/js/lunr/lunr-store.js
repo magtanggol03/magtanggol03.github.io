3744ed0aeecc3cccd874b05766c524eca3abdf8b
Ram De Guzman <rkdeguzman27@gmail.com>
2019-05-12 20:26:37 +0800

    Add overwatch post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,9 @@
 var store = [{
+        "title": "Overwatch League Stage One Season One Visualization and Analysis",
+        "excerpt":"After an exciting Stage 1, I wanted to do a bit of data analysis and visualization on the different matches in the first 5 weeks of the inaugural season of Overwatch League. Below we analyze and visualize player and team performances, character utilization and factors that affect the outcome of...","categories": ["data-viz","dataviz"],
+        "tags": [],
+        "url": "http://localhost:4000/data-viz/owl-s1s1",
+        "teaser":null},{
         "title": "Decoding Facebook Relationships",
         "excerpt":"After discovering that Facebook personal data is easily downloadable, I wanted to see if there were interesting patterns in the relationships that I had built, and to see the common threads that connect me to my different FB friends. So I decided to do a bit of topic modelling to...","categories": ["data-viz","dataviz"],
         "tags": [],
