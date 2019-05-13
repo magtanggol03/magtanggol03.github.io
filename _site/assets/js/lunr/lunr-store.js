b46c74cf3cca33d6ae4382cfa063483ba30274a7
Ram De Guzman <rkdeguzman27@gmail.com>
2019-05-13 15:02:32 +0800

    Add starwars post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,6 +4,11 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/data-viz/statdist",
         "teaser":null},{
+        "title": "Star Wars Animation Dump",
+        "excerpt":"Random animations of various elements of the Star Wars Universe.  Will keep updating this page as I get to do more!        ","categories": ["animation"],
+        "tags": [],
+        "url": "http://localhost:4000/animation/starwars-dump",
+        "teaser":null},{
         "title": "Overwatch League Stage One Season One Visualization and Analysis",
         "excerpt":"After an exciting Stage 1, I wanted to do a bit of data analysis and visualization on the different matches in the first 5 weeks of the inaugural season of Overwatch League. Below we analyze and visualize player and team performances, character utilization and factors that affect the outcome of...","categories": ["data-viz","dataviz"],
         "tags": [],
@@ -13,11 +18,6 @@ var store = [{
         "excerpt":"After discovering that Facebook personal data is easily downloadable, I wanted to see if there were interesting patterns in the relationships that I had built, and to see the common threads that connect me to my different FB friends. So I decided to do a bit of topic modelling to...","categories": ["data-viz","dataviz"],
         "tags": [],
         "url": "http://localhost:4000/data-viz/decoding-fb-friends",
-        "teaser":null},{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["animation","jekyll","update"],
-        "tags": [],
-        "url": "http://localhost:4000/hello/",
         "teaser":null},{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["illustration","jekyll","update"],
--- animation/index.md
+++ animation/index.md
@@ -1,6 +1,14 @@
 ---
-title: ""
-layout: single
+title: "Animation Projects"
+layout: splash
+feature_row:
+  - image_path: https://storage.googleapis.com/magtanggol-github-io/starwars/TieFighter.gif
+    alt: "Star Wars Animation Dump"
+    title: "Star Wars Animation Dump"
+    excerpt: "Animation of random Star Wars things"
+    url: "/animation/starwars-dump"
+    btn_label: "Read More"
+    btn_class: "btn--inverse"
 ---
 
-animation
+{% include feature_row %}
